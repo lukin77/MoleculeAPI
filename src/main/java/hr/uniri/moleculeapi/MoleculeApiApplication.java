a9d5ead36acd06fc4b28403abd240e10b42f60d9
josipluk <josip.lukin.extern@targit.at>
2023-02-03 13:41:20 +0100

    Initial commit

---
+++ src/main/java/hr/uniri/moleculeapi/MoleculeApiApplication.java
@@ -0,0 +1,13 @@
+package hr.uniri.moleculeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoleculeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoleculeApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hr/uniri/moleculeapi/MoleculeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package hr.uniri.moleculeapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoleculeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
