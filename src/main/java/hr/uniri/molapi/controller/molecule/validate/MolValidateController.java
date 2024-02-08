db359f1b9d1dd4249e607c934a40e85437101c32
jlukin <josip.lukin7@gmail.com>
2024-02-08 23:46:46 +0100

    Implement validation functions

--- src/main/java/hr/uniri/molapi/controller/molecule/validate/MolValidateController.java
+++ src/main/java/hr/uniri/molapi/controller/molecule/validate/MolValidateController.java
@@ -24,23 +24,19 @@ public class MolValidateController {
         return ResponseEntity.ok(molValidateService.isValidSmiles(smiles));
     }
 
-    //is_valid_ctab
     @PostMapping("/isValidCtab")
     public ResponseEntity<Boolean> isValidCtab(@RequestBody final String ctab) {
         return ResponseEntity.ok(molValidateService.isValidCtab(ctab));
     }
 
-    //is_valid_smarts
     @PostMapping("/isValidSmarts")
     public ResponseEntity<Boolean> isValidSmarts(@RequestBody final String smarts) {
         return ResponseEntity.ok(molValidateService.isValidSmarts(smarts));
     }
 
-    //is_valid_mol_pkl
-    @PostMapping("/isValidMolPkl")
-    public ResponseEntity<Boolean> isValidMolPk(@RequestBody final String molPkl) {
-        return ResponseEntity.ok(molValidateService.isValidMolPk(molPkl));
+    @PostMapping("/isValidMolPk")
+    public ResponseEntity<Boolean> isValidMolPk(@RequestBody final String molPk) {
+        return ResponseEntity.ok(molValidateService.isValidMolPk(molPk));
     }
-
 
 }
--- src/main/java/hr/uniri/molapi/repository/molecule/validate/MolValidateRepositoryImpl.java
+++ src/main/java/hr/uniri/molapi/repository/molecule/validate/MolValidateRepositoryImpl.java
@@ -2,34 +2,44 @@ package hr.uniri.molapi.repository.molecule.validate;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.simple.SimpleJdbcCall;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class MolValidateRepositoryImpl implements MolValidateRepository {
-    private final JdbcTemplate jdbcTemplate;
+    private static final String IS_VALID_SMILES = "is_valid_smiles";
+    private static final String IS_VALID_CTAB = "is_valid_ctab";
+    private static final String IS_VALID_SMARTS = "is_valid_smarts";
+    private static final String IS_VALID_MOL_PKL = "is_valid_mol_pkl";
+    private final SimpleJdbcCall simpleJdbcCall;
 
     @Autowired
     public MolValidateRepositoryImpl(JdbcTemplate jdbcTemplate) {
-        this.jdbcTemplate = jdbcTemplate;
+        this.simpleJdbcCall = new SimpleJdbcCall(jdbcTemplate)
+                .withSchemaName("public");
     }
 
     @Override
     public Boolean isValidSmiles(String smiles) {
-        return null;
+        return simpleJdbcCall.withFunctionName(IS_VALID_SMILES)
+                .executeFunction(Boolean.class, smiles);
     }
 
     @Override
     public Boolean isValidCtab(String ctab) {
-        return null;
+        return simpleJdbcCall.withFunctionName(IS_VALID_CTAB)
+                .executeFunction(Boolean.class, ctab);
     }
 
     @Override
     public Boolean isValidSmarts(String smarts) {
-        return null;
+        return simpleJdbcCall.withFunctionName(IS_VALID_SMARTS)
+                .executeFunction(Boolean.class, smarts);
     }
 
     @Override
     public Boolean isValidMolPk(String molPk) {
-        return null;
+        return simpleJdbcCall.withFunctionName(IS_VALID_MOL_PKL)
+                .executeFunction(Boolean.class, molPk);
     }
 }
