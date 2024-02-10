c5293f86d1100305a5e0777a14c8950fb8ac2305
jlukin <josip.lukin7@gmail.com>
2024-02-10 21:04:49 +0100

    Implement molConDesc methods

--- src/main/java/hr/uniri/molapi/controller/molecule/condesc/MolConDescController.java
+++ src/main/java/hr/uniri/molapi/controller/molecule/condesc/MolConDescController.java
@@ -1,9 +1,10 @@
 package hr.uniri.molapi.controller.molecule.condesc;
 
+import hr.uniri.molapi.model.Mol;
 import hr.uniri.molapi.service.molecule.condesc.MolConDescService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("mol/condesc")
@@ -15,4 +16,37 @@ public class MolConDescController {
     public MolConDescController(MolConDescService molConDescService) {
         this.molConDescService = molConDescService;
     }
+
+    @PostMapping("/molChiXv")
+    public ResponseEntity<Float> molChiXv(@RequestBody final Mol mol, @RequestParam(name = "X") final int x) {
+        if (x < 0 || x > 4) return ResponseEntity.badRequest().build();
+
+        return ResponseEntity.ok(molConDescService.molChiXv(mol, x));
+    }
+
+    @PostMapping("/molChiXn")
+    public ResponseEntity<Float> molChiXn(@RequestBody final Mol mol, @RequestParam(name = "X") final int x) {
+        if (x < 0 || x > 4) return ResponseEntity.badRequest().build();
+
+        return ResponseEntity.ok(molConDescService.MolChiXn(mol, x));
+    }
+
+
+    @PostMapping("/molKappaX")
+    public ResponseEntity<Float> molKappaX(@RequestBody final Mol mol, @RequestParam(name = "X") final int x) {
+        if (x < 0 || x > 3) return ResponseEntity.badRequest().build();
+
+        return ResponseEntity.ok(molConDescService.molKappaX(mol, x));
+    }
+
+    @PostMapping("/molPhi")
+    public ResponseEntity<Float> molPhi(@RequestBody final Mol mol) {
+        return ResponseEntity.ok(molConDescService.molPhi(mol));
+    }
+
+    @PostMapping("/molHallkieralpha")
+    public ResponseEntity<Float> molHallkieralpha(@RequestBody final Mol mol) {
+        return ResponseEntity.ok(molConDescService.molHallkieralpha(mol));
+    }
+
 }
